78e73fe8f4e59ba2c47ca4da7e77310e04df550c
Dr. Lakmal Rupasinghe <p.rupasinghe@postgrad.curtin.edu.au>
2023-08-07 16:23:14 +0530

    My 2n Saving

--- hack.py
+++ hack.py
@@ -1,3 +1,23 @@
-import os
-import numpy
+import socket
+
+target = "https://www.sliit.lk"
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect((target, 80))
+
+http_request = "GET / HTTP/1.1\r\nHost: " + target + "\r\n\r\n"
+s.send(http_request.encode())
+
+result = s.recv(4096).decode()
+
+print(result)
+
+s.close()
+
+
+
+
+
+
 
